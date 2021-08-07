05e847f94abec11cfb32f748ead8e36dd76ee1db
박국현 <goodpark@isujin-ui-MacBookPro.local>
2021-08-08 00:00:31 +0900

    programmers question1 complete

---
+++ algorithm/programmers/hash/question1/answer.py
@@ -0,0 +1,10 @@
+def solution(participant, completion):
+    answer = ''
+    participant.sort()
+    completion.sort()
+
+    for p, c in zip(participant, completion):
+        if p != c:
+            return p
+    answer = participant[-1]
+    return answer
